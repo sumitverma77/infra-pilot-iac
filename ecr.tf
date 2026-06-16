132d71a6f47a9e8ffccb98338e6c622f67514262
sumitverma77 <987sumitverma@gmail.com>
2026-06-16 22:33:47 +0530

    chore: make ECR repository name environment-specific

--- ecr.tf
+++ ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "this" {
-  name                 = var.project_name
+  name                 = "${var.project_name}-${var.environment}"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
